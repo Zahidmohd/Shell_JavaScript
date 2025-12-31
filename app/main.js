23ceb91ede041e44b6739208f1e316adc392f66d
Zahidmohd <btech25082.21@bitmesra.ac.in>
2025-12-31 21:00:17 +0530

    printing error messages for invalid commands

--- app/main.js
+++ app/main.js
@@ -5,7 +5,10 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+
+
 // TODO: Uncomment the code below to pass the first stage
-rl.question("$ ", (answer) => {
+rl.question("$ ", (command) => {
+  console.log(`${command}: command not found`);
   rl.close();
 });
